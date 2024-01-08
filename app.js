f9b8144f6f226d6fb6ceffbf069fc5ea58ee8a92
yuan6636 <aleniverson777@gmail.com>
2024-01-08 15:41:58 +0800

    use dotenv

--- app.js
+++ app.js
@@ -10,13 +10,17 @@ const routes = require('./routes')
 const app = express()
 const port = process.env.PORT || 3000
 
+if(process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
 app.engine('.hbs', engine({ extname: '.hbs', helpers: handlebarsHelpers }))
 app.set('view engine', '.hbs')
 app.set('views', './views')
 
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
-app.use(session({ secret: 'ThisIsSecret', resave: false, saveUninitialized: false }))
+app.use(session({ secret: process.env.SESSION_SECRET, resave: false, saveUninitialized: false }))
 app.use(flash())
 
 app.use(messageHandler)
